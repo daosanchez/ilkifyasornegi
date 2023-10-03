7c7736915e627d11d0808694548987cbc107e0bc
ravza <145916188+ravzasanchez@users.noreply.github.com>
2023-10-03 14:55:25 +0300

    Add files via upload

---
+++ form16/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMesaj_Click(object sender, EventArgs e)
+        {
+            int yas = 32;
+            if(yas >= 18)
+            {
+                MessageBox.Show("Ehliyet Alabilirsiniz");
+            }
+
+            if(yas < 18)
+            {
+                MessageBox.Show("Üzgünüm Biraz Daha Büyümelisin");
+            }
+        }
+    }
+}
